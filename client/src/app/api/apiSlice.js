b51661e6bc1c3f0c99322348ae923baa7fdcde9a
S-P-Ks <kumbhareshubham9@gmail.com>
2023-06-22 19:04:13 +0530

    6 commit

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -7,7 +7,7 @@ const baseQuery = fetchBaseQuery({
     const token = getState().auth.token;
 
     if (token) {
-      headers.set("authorisation", `Bearer ${token}`);
+      headers.set("authorization", `Bearer ${token}`);
     }
     return headers;
   },
