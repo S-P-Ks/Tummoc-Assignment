9a88fe04d59f5d1da6d4c29e2daddbe19e9b15f7
S-P-Ks <kumbhareshubham9@gmail.com>
2023-06-22 19:16:10 +0530

    8 commit

--- server/index.js
+++ server/index.js
@@ -77,6 +77,7 @@ app.post("/upload", multer.single("file"), async (req, res) => {
       blobStream.end(req.file.buffer);
     } else throw "error with img";
   } catch (error) {
+    console.log(error);
     res.status(500).send(error);
   }
 });
